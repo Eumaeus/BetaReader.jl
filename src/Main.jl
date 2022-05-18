0c5355d4417549149ff9e018063fd378cc22dc64
Eumaeus <cwblackwell@gmail.com>
2022-05-18 12:20:20 -0400

    Progress

--- src/Main.jl
+++ src/Main.jl
@@ -2,3 +2,91 @@ greet() = print("Hello World!")
 
 greetMe() = "Hello, Boss."
 
+# Functions for identifying classes of characters
+
+function isBreathing(c)
+    breathings = "()"
+    if (contains(breathings, c))
+        true
+    else
+        false
+    end
+end
+
+function isAccent(c)
+    accents = "/\\="
+    if (contains(accents, c))
+        true
+    else
+        false
+    end
+end
+
+function isIotaSubscript(c)
+    if (c == "|") true
+    else false
+    end
+end
+
+function isDiaeresis(c)
+    if (c == "+") true
+    else false
+    end
+end
+
+function isPunctuation(c)
+    puncts = """,."';[]-—:"""
+    if (contains(puncts,c)) true
+    else false
+    end
+end
+
+
+function isValidLCConsonant(c)
+    validLCConsonants = "bgdzqklmncprstfxy"
+    if (contains(validLCConsonants,c)) true
+    else false
+    end
+end
+
+function isSigmaTerminator(c)
+    sigmaTerms = """,.":;— \t\n"""
+    if (contains(sigmaTerms, c)) true
+    else false
+    end
+end
+
+
+function isValidUCConsonant(c)
+    validLCConsonants = "BGDZQKLMNCPRSTFXY"
+    if (contains(validUCConsonants,c)) true
+    else false
+    end
+end
+
+function isValidLCVowel(c)
+    validLCVowels = "aehiouw"
+    if (contains(validLCVowels, c)) true
+    else false
+    end
+end
+
+function isValidUCVowel(c)
+    validUCVowels = "AEHIOUW"
+    if (contains(validLCVowels, c)) true
+    else false
+    end
+end
+
+function isUCMarker(c)
+    if c == "*" true
+    else false
+    end
+end
+
+function isDiphthong(s)
+    allDiphthongs = ["ai", "ei", "oi", "ui", "au", "eu", "hu", "ou", "Ai", "Ei", "Oi", "Ui", "Au", "Eu", "Hu", "Ou"]
+    if (s in(allDiphthongs)) true
+    else false
+    end
+end
--- test/test_basic.jl
+++ test/test_basic.jl
@@ -1,6 +1,8 @@
 
+
 @testset "BasicTests" begin
-  
+
+ #=
  @test 1 == 1
 
  @test_broken 3 == 4
@@ -10,5 +12,31 @@
  @test_broken "abc" == "abc"
 
  @test BetaReader.greetMe() == "Hello, Boss!"
+ =#
 
+ # Read tests
+
+ @test BetaReader.isBreathing("(")
+ @test BetaReader.isBreathing(")")
+ @test_broken BetaReader.isBreathing("a")
+@test BetaReader.isAccent("/")
+@test BetaReader.isAccent("\\")
+@test BetaReader.isAccent("=")
+@test_broken BetaReader.isAccent("(")
+
+@test BetaReader.isIotaSubscript("|")
+@test BetaReader.isDiaeresis("+")
+
+@test BetaReader.isDiphthong("ai")
+@test BetaReader.isDiphthong("ei")
+@test BetaReader.isDiphthong("ou")
+@test_broken BetaReader.isDiphthong("hi")
+@test_broken BetaReader.isDiphthong("io")
+@test BetaReader.isDiphthong("Ai")
+@test BetaReader.isDiphthong("Ei")
+@test BetaReader.isDiphthong("Ou")
+@test_broken BetaReader.isDiphthong("Hi")
+@test_broken BetaReader.isDiphthong("Io")
+
+@test BetaReader.isSigmaTerminator(" ")
 end
